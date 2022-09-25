80554f24c1b1cddaf98de3920258fcb80c9b2ca6
JulianH <jherreilers@gmail.com>
2022-09-26 00:06:11 +0200

    Further cleaning up of implementation of Bshall

--- tacotron/text.py
+++ tacotron/text.py
@@ -14,17 +14,6 @@ SYMBOLS = [
 ]
 # fmt: on
 
-# SYMBOLS = [
-#     '_', '~', ' ', *PUNCTUATION, 'AA', 'AA0', 'AA1', 'AA2', 'AE', 'AE0',
-#     'AE1', 'AE2', 'AH', 'AH0', 'AH1', 'AH2', 'AO', 'AO0', 'AO1', 'AO2', 'AW',
-#     'AW0', 'AW1', 'AW2', 'AY', 'AY0', 'AY1', 'AY2', 'B', 'CH', 'D', 'DH', 'EH',
-#     'EH0', 'EH1', 'EH2', 'ER', 'ER0', 'ER1', 'ER2', 'EY', 'EY0', 'EY1', 'EY2',
-#     'F', 'G', 'HH', 'IH', 'IH0', 'IH1', 'IH2', 'IY', 'IY0', 'IY1', 'IY2', 'JH',
-#     'K', 'L', 'M', 'N', 'NG', 'OW', 'OW0', 'OW1', 'OW2', 'OY', 'OY0', 'OY1',
-#     'OY2', 'P', 'R', 'S', 'SH', 'T', 'TH', 'UH', 'UH0', 'UH1', 'UH2', 'UW',
-#     'UW0', 'UW1', 'UW2', 'V', 'W', 'Y', 'Z', 'ZH'
-# ]
-
 symbol_to_id = {s: i for i, s in enumerate(SYMBOLS)}
 id_to_symbol = {i: s for i, s in enumerate(SYMBOLS)}
 
@@ -117,20 +106,6 @@ def load_afrdict():
         entry = " ".join([str(word) for word in dict_list[i][1:]])
         afrdict[str(dict_list[i][0])] = entry
     return afrdict
-
-
-    # dict_ref = "rcrl_apd.1.4.1.txt"
-    # with open(dict_ref, encoding="ISO-8859-1") as file:
-    #     afrdict = (line.strip().split("  ") for line in islice(file, 126, 133905))
-    #     print(afrdict)
-    #     afrdict = {
-    #         format_alt_entry(word): pronunciation for word, pronunciation in afrdict
-    #     }
-    #     print(type(afrdict))
-    #     print(afrdict[0])
-    #     print(type(afrdict[0]))
-    # return afrdict
-
 
 def parse_text(text, afrdict):
     words = tokenize(clean(text))
